8c008f940d7651afda3da0aac2ab79f5fdd1c3b4
blessingtutka <blessingtutka298@gmail.com>
2025-10-21 08:36:51 +0200

    feat(test): add ghost units test

--- jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -1,42 +1,121 @@
 package nl.tudelft.jpacman.npc.ghost;
 
-import nl.tudelft.jpacman.board.BoardFactory;
-import nl.tudelft.jpacman.board.Direction;
+
+import java.util.*;
+
+import nl.tudelft.jpacman.board.*;
 import nl.tudelft.jpacman.level.*;
+import nl.tudelft.jpacman.npc.Ghost;
 import nl.tudelft.jpacman.sprite.PacManSprites;
-import org.junit.jupiter.api.BeforeEach;
+import nl.tudelft.jpacman.sprite.Sprite;
+import nl.tudelft.jpacman.npc.ghost.Clyde;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import java.awt.image.DirectColorModel;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Test various aspects of clyde.
+ *
+ * @author Blessing Tutka
+ */
 class ClydeTest {
-    private PacManSprites pacManSprites = new PacManSprites();
-    private PlayerFactory playerFactory = new PlayerFactory(pacManSprites);
-    private GhostFactory ghostFactory = new GhostFactory(pacManSprites);
-    private LevelFactory levelFactory = new LevelFactory(pacManSprites, ghostFactory);
-    private BoardFactory boardFactory = new BoardFactory(pacManSprites);
-    MapParser ghostMapParser = new GhostMapParser(levelFactory, boardFactory, ghostFactory);
+
+    private final PacManSprites pacmans = new PacManSprites();
+
+    private final BoardFactory bf  = new BoardFactory(pacmans);
+    private final GhostFactory gf = new GhostFactory(pacmans);
+    private final LevelFactory lf  = new LevelFactory(pacmans, gf);
+    private final PlayerFactory pf = new PlayerFactory(pacmans);
+
+    private final MapParser mapParser = new GhostMapParser(lf,bf,gf);
+
+    //private final Unit nearest = Navigation.findNearest(Player.class, hyde.getSquare());
+    //private final Square target = nearest.getSquare();
+    //private final List<Direction> path = Navigation.shortestPath(hyde.getSquare(), target, hyde);
+
+    /**
+     * Verifies the board has the correct width.
+     */
+    @Test
+    void verifynextAiMoveFarPathFree() {
+        //Map
+        char[][] map = {
+            "############".toCharArray(),
+            "#P        C#".toCharArray(),
+            "############".toCharArray()
+        };
+        // Setup
+        Level level = mapParser.parseMap(map);
+        Board board = level.getBoard();
+
+        Ghost hyde = (Clyde) Navigation.findUnitInBoard(Ghost.class,board);
+
+        Player player = pf.createPacMan();
+        player.setDirection(Direction.EAST);
+        level.registerPlayer(player);
+
+        Optional<Direction>[] directionPossibilities = new Optional[]{
+            Optional.of(Direction.NORTH),
+            Optional.of(Direction.SOUTH),
+            Optional.of(Direction.WEST)
+        };
+        System.out.println(hyde.nextAiMove());
+        //Test
+        assertThat(hyde.nextAiMove()).isIn(directionPossibilities);
+    }
 
     @Test
-    void distanceGreaterThan8AndPathBlockedTest() {
-        List<String> map = Arrays.asList(
-            "#############",
-            "#C#        P#",
-            "#############"
-        );
+    void verifynextAiMoveNearPathFree() {
+        //Map
+        char[][] map = {
+            "##############".toCharArray(),
+            "#P          C#".toCharArray(),
+            "##############".toCharArray()
+        };
+        // Setup
+        Level level = mapParser.parseMap(map);
+        Board board = level.getBoard();
 
-        Level level = ghostMapParser.parseMap(map);
-        Player pacman = playerFactory.createPacMan();
-        level.registerPlayer(pacman);
+        Ghost hyde = (Clyde) Navigation.findUnitInBoard(Ghost.class,board);
 
-        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, level.getBoard());
-        assertNotNull(clyde);
-        Optional<Direction> direction = clyde.nextAiMove();
-        assertEquals(Optional.empty(), direction);
+        Player player = pf.createPacMan();
+        player.setDirection(Direction.WEST);
+        level.registerPlayer(player);
+
+        System.out.println(hyde.nextAiMove());
+
+        System.out.println(board);
+        //Test
+        assertThat(hyde.nextAiMove()).isEqualTo(Optional.of(Direction.WEST));
     }
+
+    @Test
+    void verifynextAiMovePathBlock() {
+        //Map
+        char[][] map = {
+            "############".toCharArray(),
+            "#P       #C#".toCharArray(),
+            "############".toCharArray()
+        };
+        // Setup
+        Level level = mapParser.parseMap(map);
+        Board board = level.getBoard();
+
+        Ghost hyde = (Clyde) Navigation.findUnitInBoard(Ghost.class,board);
+
+        Player player = pf.createPacMan();
+        player.setDirection(Direction.EAST);
+        level.registerPlayer(player);
+
+        //Test
+        assertThat(hyde.nextAiMove()).isEqualTo(Optional.empty());
+    }
+
+    /**
+     * Verifies the board has the correct height.
+     */
 }
